e3996088a77733f3395cef50880717242a8ee18b
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2013-04-21 02:03:10 +0200

    Commented

--- Eve.API/Vision/FaceDetectionProvider.cs
+++ Eve.API/Vision/FaceDetectionProvider.cs
@@ -17,18 +17,24 @@ using Accord.Vision.Tracking;
 using Eve.Core.Loging;
 
 namespace Eve.API.Vision {
+	/// <summary>
+	/// Face detection and tracking provider
+	/// Provides video source too
+	/// </summary>
 	public class FaceDetectionProvider : IDisposable {
 		// TODO Call face detection periodically
-		private Log.LogInstance log =
+		// TODO Constants to properties
+		// TODO Separate video source from detection provider
+		private readonly Log.LogInstance log =
 			new Log.LogInstance(typeof(FaceDetectionProvider));
 
 		// Video Source variables
 		public int VideoSourceWidth = 320;
 		public int VideoSourceHeight = 240;
 		public int VideoSourceStopWaitTime = 2000;
+		public const int FramesToSkip = 10;
 		private VideoCaptureDevice videoSource;
-		private int framesSkipped = 0;
-		private const int FramesToSkip = 10;
+		private int framesSkipped;
 
 		// Face Tracking variables
 		public double DetectingFrameWidth = 160.0;
@@ -40,13 +46,27 @@ namespace Eve.API.Vision {
 		private bool isDetectingInProgress;
 
 
+		/// <summary>
+		/// Object constructor
+		/// </summary>
 		public FaceDetectionProvider() {
 			this.log.Info("Object instance created");
 
-			this.trackers = new List<FaceTracker>();
+			this.Reset();
+		}
+
+		/// <summary>
+		/// Object destructor
+		/// </summary>
+		~FaceDetectionProvider() {
+			this.log.Info("Object instance destroyed");
 		}
 
 
+		/// <summary>
+		/// Initializes video source and detection components
+		/// </summary>
+		/// <returns>Returns asynchronous void Task</returns>
 		public async Task InitializeAsync() {
 			this.log.Info("Initialization started...");
 			var sw = new System.Diagnostics.Stopwatch();
@@ -63,38 +83,45 @@ namespace Eve.API.Vision {
 			this.log.Info("Initialization successful in {0}ms", sw.ElapsedMilliseconds);
 		}
 
-		private async Task InitializeVideoSourceAsync() {
+		/// <summary>
+		/// Looks for available video sources 
+		/// </summary>
+		/// <returns>Returns asynchronous void Task</returns>
+		private async Task InitializeVideoSourceAsync(string monkierString = null) {
 			// Get available video sources
 			var availableVideoSources =
 				new FilterInfoCollection(FilterCategory.VideoInputDevice);
 			if (availableVideoSources.Count <= 0)
 				throw new Exception("No video devices available");
 
-			// Create video source object
-			this.videoSource =
-				new VideoCaptureDevice(availableVideoSources[0].MonikerString) {
-					DesiredFrameSize = new Size(this.VideoSourceWidth, this.VideoSourceHeight)
-				};
+			// Create video source object from first found or of given name
+			try {
+				this.videoSource =
+					new VideoCaptureDevice(monkierString ??
+										   availableVideoSources[0].MonikerString) {
+											   DesiredFrameSize =
+												   new Size(this.VideoSourceWidth, this.VideoSourceHeight)
+										   };
+			}
+			catch (Exception ex) {
+				this.log.Error<Exception>(ex, "Couldn't create video capture device");
+				return;
+			}
 
 			// Wait for video source to stop
 			await this.StopVideoSource();
 		}
 
-		private async Task InitializeDetectorsAsync() {
-			await Task.Run(() => {
-				this.log.Info("Initializing face detector...");
-
-				// InitializeAsync face detector for multiple objects
-				this.detector = new HaarObjectDetector(
-					new FaceHaarCascade(),
-					this.DetectingAreaMin, ObjectDetectorSearchMode.NoOverlap,
-					this.DetectingAreaStep, ObjectDetectorScalingMode.GreaterToSmaller) {
-						UseParallelProcessing = true
-					};
-			});
-		}
-
+		/// <summary>
+		/// Stops video source signal if already not stopped;
+		/// </summary>
+		/// <returns>Returns asynchronous void Task</returns>
 		private async Task StopVideoSource() {
+			if (this.videoSource == null) {
+				throw new NullReferenceException(
+					"Can't stop video source if null. videoSource is null");
+			}
+
 			// Signal camera to stop recording
 			this.videoSource.SignalToStop();
 
@@ -113,6 +140,29 @@ namespace Eve.API.Vision {
 				this.videoSource.Stop();
 		}
 
+		/// <summary>
+		/// Creates Face Haar detector
+		/// </summary>
+		/// <returns>Returns asynchronous void Task</returns>
+		private async Task InitializeDetectorsAsync() {
+			await Task.Run(() => {
+				this.log.Info("Initializing face detector...");
+
+				// InitializeAsync face detector for multiple objects
+				this.detector = new HaarObjectDetector(
+					new FaceHaarCascade(),
+					this.DetectingAreaMin, ObjectDetectorSearchMode.NoOverlap,
+					this.DetectingAreaStep, ObjectDetectorScalingMode.GreaterToSmaller) {
+						UseParallelProcessing = true
+					};
+			});
+		}
+
+		/// <summary>
+		/// Processes given frame - detecting faces and tracking objects
+		/// </summary>
+		/// <param name="frame">Frame to process</param>
+		/// <returns>Returns asynchronous void Task</returns>
 		public async Task ProcessFrameAsync(UnmanagedImage frame) {
 			// Skip first few frames during initialization
 			if (this.framesSkipped <= FramesToSkip) {
@@ -125,9 +175,13 @@ namespace Eve.API.Vision {
 				this.DetectFacesAsync(frame);
 
 			// Track all active faces
-			await this.TrackFacesAsync(frame);
+			this.TrackFaces(frame);
 		}
 
+		/// <summary>
+		/// Detects face regions and creates trackers
+		/// </summary>
+		/// <param name="image">Image to search for faces on</param>
 		private async void DetectFacesAsync(UnmanagedImage image) {
 			var sw = new System.Diagnostics.Stopwatch();
 			sw.Start();
@@ -159,6 +213,14 @@ namespace Eve.API.Vision {
 						  regions.Length, sw.ElapsedMilliseconds);
 		}
 
+		/// <summary>
+		/// Creates new face tracker
+		/// Recycles inactive trackers
+		/// </summary>
+		/// <param name="trackingRegion">Region of image to track</param>
+		/// <param name="xScale">X scale of downsampled image to original image</param>
+		/// <param name="yScale">Y scale if downsampled image to original image</param>
+		/// <param name="image">Downsampled image from which we trace given rectangle</param>
 		private void CreateFaceTracker(Rectangle trackingRegion, double xScale,
 									   double yScale, UnmanagedImage image) {
 			// Find first not active tracker 
@@ -193,13 +255,23 @@ namespace Eve.API.Vision {
 			faceTracker.Tracker.ProcessFrame(image);
 		}
 
-		private async Task TrackFacesAsync(UnmanagedImage image) {
+		/// <summary>
+		/// Processes all available trackers for face on given frame
+		/// </summary>
+		/// <param name="image">Frame to process for trackers</param>
+		private void TrackFaces(UnmanagedImage image) {
 			// Do tracking for all trackers
 			Parallel.ForEach(this.trackers, async (tracker) => {
 				await this.TrackFaceAsync(tracker, image);
 			});
 		}
 
+		/// <summary>
+		/// Processes given frame for given face tracker
+		/// </summary>
+		/// <param name="faceTracker">Face tracker to process</param>
+		/// <param name="image">Frame to process for tracker</param>
+		/// <returns>Returns asynchronous void Task</returns>
 		private async Task TrackFaceAsync(FaceTracker faceTracker, UnmanagedImage image) {
 			// Check if face tracker is active
 			if (!faceTracker.IsActive) return;
@@ -218,29 +290,55 @@ namespace Eve.API.Vision {
 			}
 		}
 
+		private void Reset() {
+			// Set values to default
+			if (this.trackers != null)
+				this.trackers.ForEach(t => t.IsActive = false);
+			else this.trackers = new List<FaceTracker>();
+			this.framesSkipped = 0;
+		}
+
 
 		#region Properties
 
+		/// <summary>
+		/// Gets video source of provider
+		/// </summary>
 		public IVideoSource Source {
 			get { return this.videoSource; }
 		}
 
+		/// <summary>
+		/// Gets all trackers active and inactive
+		/// </summary>
 		public IEnumerable<FaceTracker> Trackers {
 			get { return this.trackers; }
 		}
 
 		#endregion
 
+		/// <summary>
+		/// Disposes (stops) video source if available
+		/// </summary>
 		public async void Dispose() {
-			await this.StopVideoSource();
+			if (this.videoSource != null)
+				await this.StopVideoSource();
 		}
 
+		/// <summary>
+		/// FaceTracker class used for tracking one face object
+		/// Contains marker for easy development
+		/// </summary>
 		public class FaceTracker {
 			public bool IsActive { get; set; }
 			public Camshift Tracker { get; private set; }
 			public RectanglesMarker Marker { get; private set; }
 
 
+			/// <summary>
+			/// Object constructor
+			/// Created RGB tracker and empty rectangle marker
+			/// </summary>
 			public FaceTracker() {
 				// Create tracker
 				this.Tracker = new Camshift() {
@@ -254,10 +352,18 @@ namespace Eve.API.Vision {
 			}
 
 
+			/// <summary>
+			/// Creates new rectangle of current tracker search window
+			/// </summary>
 			private void UpdateMarkers() {
 				this.Marker = new RectanglesMarker(this.Tracker.SearchWindow);
 			}
 
+			/// <summary>
+			/// Draws rectangle around tracking object
+			/// </summary>
+			/// <param name="image">Image to add markers to</param>
+			/// <param name="drawAxis">Whether to draw X axis line</param>
 			public void DrawMarker(ref UnmanagedImage image, bool drawAxis = false) {
 				// Update marker
 				this.UpdateMarkers();
