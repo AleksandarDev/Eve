ca938329f53ca3506e6f136a6ee87440b820b6f1
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2013-06-14 22:03:59 +0200

    Updated for internet support
    now over real internet, not virtual network

--- Eve Control/Windows/Vision/VisionViewModel.cs
+++ Eve Control/Windows/Vision/VisionViewModel.cs
@@ -6,5 +6,26 @@ using System.Threading.Tasks;
 using Eve.Core.ViewModels;
 
 namespace EveControl.Windows.Vision {
-	public class VisionViewModel : NotificationObject {}
+	public class VisionViewModel : NotificationObject, IDisposable {
+		#region IDisposable implementation
+
+		private bool isDisposed;
+
+		public void Dispose() {
+			this.Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		protected async virtual void Dispose(bool disposing) {
+			if (this.isDisposed) return;
+
+			if (disposing) {
+				// Dispose any disposable objects HERE
+			}
+
+			this.isDisposed = true;
+		}
+
+		#endregion
+	}
 }
